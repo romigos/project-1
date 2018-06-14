e75ef4ca2135e791ebaafdcfdf5bb6f4508b7c82
Roman <bolist@mail.ru>
2018-06-14 15:26:57 +0300

    шапка

---
+++ js/script.js
@@ -0,0 +1,7 @@
+menu.onclick = function myFunction() {
+    let x = doument.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += "responsive"; }
+    else {
+        x.className = "topnav"; }
+}
